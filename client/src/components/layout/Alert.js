c532a2b320ea6acab46af20df3ccbb42951da0a3
Mike Bolloskis <mpb0406@gmail.com>
2021-08-26 21:04:46 -0400

    Added Auth Reducer and Action to Make Post Request to Server and Create User

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
     <div key={alert.id} className={`alert alert-${alert.alertType}`}>
         {alert.msg}
-        <p>X</p>
     </div>
 ))
 
