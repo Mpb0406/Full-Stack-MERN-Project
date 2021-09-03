70260003bdc9806eced91225de3f2711b5881f0b
Mike Bolloskis <mpb0406@gmail.com>
2021-09-02 20:52:10 -0400

    Started Dashboard - Added Spinner Component While Loading

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,13 +2,28 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../layout/Spinner";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <div>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
+      </p>
+      {profile !== null ? <div>has</div> : <div>hasn't</div>}
+    </div>
+  );
 };
 
 Dashboard.propTypes = {
---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "center", margin: "20vh 0" }}
+    >
+      <i class="fa fa-spinner fa-spin fa-4x"></i>
+    </div>
+  );
+};
+
+export default Spinner;
