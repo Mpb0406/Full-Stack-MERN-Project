cc0f8c17400730de8c40378ba08a83e80bf6f24d
Mike Bolloskis <mpb0406@gmail.com>
2021-09-03 21:34:56 -0400

    Added Create Profile and Edit Profile Components and Hooked Up Redux Actions to Add and Edit Profile Information

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/Dashboard/Dashboard";
 import PrivateRoute from "./components/rounting/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -40,6 +41,11 @@ const App = () => {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
               />
             </Switch>
           </section>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
 
-// get Current User's Profile
+// Get Current User's Profile
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/profile/me");
@@ -18,3 +18,42 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or Update Profile
+export const createProfile =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await axios.post("/api/profile", formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(
+        setAlert(edit ? "Profile updated" : "Profile created", "success")
+      );
+
+      if (!edit) {
+        history.push("/dashboard");
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  };
